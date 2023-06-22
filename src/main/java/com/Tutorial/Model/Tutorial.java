5aa5092bf17c9659930ff3aca4fd15e2849310f9
Dinesh Gupta <Dineshg0009@gmail.com>
2023-06-22 12:29:08 +0530

    Initial commit

---
+++ src/main/java/com/Tutorial/Model/Tutorial.java
@@ -0,0 +1,30 @@
+package com.Tutorial.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tutorials")
+public class Tutorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "published")
+    private boolean published;
+
+    public Tutorial(String title, String description, boolean b) {
+        this.title= title;
+        this.description=description;
+        this.published=isPublished();
+    }
+}
